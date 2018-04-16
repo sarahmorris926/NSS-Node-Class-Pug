560d2c187fb0bc0c1056aaf92f794c136e8359d4
Sarah Morris <sarah.morris9@gmail.com>
2018-04-16 14:18:43 -0500

    Added templating with pug

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const names = ["Moe", "Larry", "Barbara"];
 
 app.set("view engine", "pug");
+
+app.use(express.static(__dirname + "/public"));
+app.use("/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist/"));
+
+app.get("/", (req, res, next) => {
+  let homeData = {
+    subtitle: "This came from my JS data",
+    names
+  }
+  res.render("index", homeData);
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
